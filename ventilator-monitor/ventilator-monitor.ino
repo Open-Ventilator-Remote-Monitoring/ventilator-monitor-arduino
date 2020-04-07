7a8103edc58a2bf4136d6769f4a38f0e77296199
Matt <matthew.allen.william@gmail.com>
2020-04-07 15:20:47 -0400

    First working Arduino library.

--- ventilator-monitor/ventilator-monitor.ino
+++ ventilator-monitor/ventilator-monitor.ino
@@ -1,65 +1,41 @@
 /*
-Remote Ventilator
+Remote Ventilator Usage Demo.
 
 Demo code only
 
 */
+#include "OpenMonitor.h"
+
+int ieRatio = 0;
+int peakInspiratoryPressure = 0;
+int tidalVolume = 0;
+int respiratoryRate = 0;
+int peep = 0;
 
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup() 
+{  
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+
+  //Initialize OpenMonitor library.
+  omInit();
 }
 
 // the loop function runs over and over again forever
-void loop() {
-
-  if (Serial.available() > 0) {
-
-    String data = Serial.readStringUntil('\n');
-
-    //Serial.println('Recieved: ' + data);
-
-    if(data == "getStats") {
-
-      int ieRatio = random(1, 4);
-      int peakInspiratoryPressure = random(60, 80);
-      int peep = random(5, 10);
-      int respiratoryRate = random(8, 35);
-      int tidalVolume = random(300, 800);
-      
-      String output = "";
-    
-      output += "{";
-      output +=   "\"ventilator\": [";
-      output +=     "{";
-      output +=       "\"ieRatio\": \"1:";
-      output +=         ieRatio;
-      output +=       "\",";
-      output +=       "\"peakInspiratoryPressure\": \"";
-      output +=         peakInspiratoryPressure;
-      output +=         "\",";
-      output +=       "\"peep\": \"";
-      output +=         peep;
-      output +=       "\",";
-      output +=       "\"respiratoryRate\": \"";
-      output +=         respiratoryRate;
-      output +=       "\",";
-      output +=       "\"tidalVolume\": \"";
-      output +=         tidalVolume;
-      output +=       "\"";
-      output +=     "}";
-      output +=   "]";
-      output += "}";
+void loop() 
+{
+  ieRatio = random(1, 4);
+  peakInspiratoryPressure = random(300, 800);
+  tidalVolume = random(300, 800);
+  respiratoryRate = random(8, 35);
+  peep = random(5, 10);
   
-      Serial.println(output);
+  omUpdateVentilatorData(ieRatio, peakInspiratoryPressure, tidalVolume, respiratoryRate, peep); //Update OpenMonitor ventilator data buffer.
+  omAutoReport();
   
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(100);                       // wait for a second
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-     
-    }    
-  }
+  delay(500);
 }
